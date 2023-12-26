c6bb6a7f82a64080d8a830129fb8eba764c42916
tsabudh <tsabudh@gmail.com>
2023-12-26 08:35:35 +0545

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import React from 'react';
 import Dashboard from './pages/Dashboard/Dashboard';
 import RegisterPane from './components/RegisterPane/RegisterPane';
 
