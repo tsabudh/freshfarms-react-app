1462fc5de8b34ad9ae09e36ff1456a4066ce3d40
tsabudh <tsabudh@gmail.com>
2024-01-13 09:40:52 +0545

    build;

--- src/components/Notifier/Notifier.jsx
+++ src/components/Notifier/Notifier.jsx
@@ -1,21 +1,22 @@
+import React from 'react';
 import { ToastContainer, Zoom } from 'react-toastify';
-const Notifier = function(){
-    return(
+const Notifier = function () {
+    return (
         <ToastContainer
-        transition={Zoom}
-        position="top-right"
-        autoClose={1000}
-        limit={1}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover
-        theme="colored"
+            transition={Zoom}
+            position="top-right"
+            autoClose={1000}
+            limit={1}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable={false}
+            pauseOnHover
+            theme="colored"
         />
-    )
-}
+    );
+};
 
 export default Notifier;
