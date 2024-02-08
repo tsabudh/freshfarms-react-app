cdd0e6a0c6a141db1c60a72c6800ac6c2fd22305
tsabudh <tsabudh@gmail.com>
2024-02-08 22:55:15 +0545

    refreshToken when login

---
+++ src/utils/refreshToken.js
@@ -0,0 +1,28 @@
+import API_ROUTE from '../assets/globals/baseRoute';
+
+export async function refreshToken(token) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let xhttp = new XMLHttpRequest();
+            let apiRoute = `${API_ROUTE}/api/v1/admins/refreshToken`;
+
+            xhttp.onreadystatechange = () => {
+                if (xhttp.readyState == 4) {
+                    console.log('Request COmpleted');
+                    let response = JSON.parse(xhttp.responseText);
+                    resolve(response);
+                }
+            };
+
+            xhttp.open('GET', apiRoute, true);
+            xhttp.setRequestHeader('Content-Type', 'application/json');
+            xhttp.setRequestHeader('Authorization', `Bearer ${token}`);
+
+            xhttp.send();
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+export default refreshToken;
