3dfb4f0da36c1308a4db0bce64425b643beb4a88
Sabudh Thapa <tsabudh@gmail.com>
2025-09-16 18:38:22 +0545

    chore(navbar): removed store from homepage navbar

--- src/components/NavBarHome/NavBarHome.tsx
+++ src/components/NavBarHome/NavBarHome.tsx
@@ -1,35 +1,32 @@
-import classNames from 'classnames/bind';
-import React from 'react';
+import classNames from "classnames/bind";
+import React from "react";
 
-import { NavLink } from 'react-router-dom';
-import styles from './NavBarHome.module.scss';
-import Logo from '../UI/Icons/Logo';
+import { NavLink } from "react-router-dom";
+import styles from "./NavBarHome.module.scss";
+import Logo from "../UI/Icons/Logo";
 const cx = classNames.bind(styles);
 
 function NavBarHome() {
-    return (
-        <section className={cx('navbar')}>
-            <div className={cx('brand')}>
-                <NavLink to="/">
-                    <Logo cx={cx} />
-                </NavLink>
-            </div>
-            <nav className={styles['navbar_menu']}>
-                <ul>
-                    <li>
-                        <NavLink to="/store">store</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact">contact</NavLink>
-                    </li>
+  return (
+    <section className={cx("navbar")}>
+      <div className={cx("brand")}>
+        <NavLink to="/">
+          <Logo cx={cx} />
+        </NavLink>
+      </div>
+      <nav className={styles["navbar_menu"]}>
+        <ul>
+          <li>
+            <NavLink to="/contact">contact</NavLink>
+          </li>
 
-                    <li>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-    );
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 }
 
 export default NavBarHome;
